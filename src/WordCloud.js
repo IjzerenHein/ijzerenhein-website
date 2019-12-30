9dfcb49b045857da687340cd85b74b44aa8ef9df
Hein Rutjes <hrutjes@gmail.com>
2019-12-30 21:22:16 +0100

    feat: added new words

--- src/WordCloud.js
+++ src/WordCloud.js
@@ -3,98 +3,108 @@ import TagCloud from 'react-tag-cloud';
 import Word3D from './Word3D';
 
 const styles = {
-  default: {
-    fontFamily: 'sans-serif',
-    fontSize: 30,
-    fontStyle: 'italic',
-    padding: 4,
-  },
-  large: {
-    fontSize: 60,
-    fontWeight: 'bold',
-  },
-  small: {
-    fontSize: 16,
-  },
+	default: {
+		fontFamily: 'sans-serif',
+		fontSize: 30,
+		fontStyle: 'italic',
+		padding: 4,
+	},
+	large: {
+		fontSize: 60,
+		fontWeight: 'bold',
+	},
+	small: {
+		fontSize: 16,
+	},
 };
 
 const words = {
-  large: ['react-native', 'JavaScript', 'React.js', 'Arion Run', 'Firestorter'],
-  normal: [
-    'iOS',
-    'Android',
-    'Objective-C',
-    'Animations',
-    'Java',
-    'Github',
-    'Native',
-    'Full stack',
-    'Creative',
-    'Architect',
-    'Bitbucket',
-    'Firebase',
-    'Circle-CI',
-    'autolayout.js',
-    'node-web-bluetooth',
-    'react-tag-cloud',
-    'Node.js',
-    'Web Bluetooth',
-    'famous',
-    'HTML',
-    'CSS',
-    'ES6',
-    'Async/await',
-    'eslint',
-    'Flow-Xpert',
-    'FlowX',
-    'UX',
-    'Mocha',
-    'Jest',
-    'C++',
-    'SQL',
-    'YAML',
-    'XCode',
-    'Rouwcollectie',
-    'Git',
-    'TDD',
-    'TypeScript',
-  ],
-  small: ['Cordova', 'MFC', '.NET', 'VBA', 'Win-CE', 'Eindhoven', 'Strijp-S'],
+	large: [
+		'react-native',
+		'JavaScript',
+		'react-native-shared-element',
+		'Vicky Parking',
+		'Arion Run',
+		'Firestorter',
+	],
+	normal: [
+		'react-native-magic-move',
+		'react-native-bundle-visualizer',
+		'React.js',
+		'iOS',
+		'Android',
+		'Objective-C',
+		'Animations',
+		'Java',
+		'Github',
+		'Native',
+		'Full stack',
+		'Creative',
+		'Architect',
+		'Bitbucket',
+		'Firebase',
+		'Circle-CI',
+		'autolayout.js',
+		'node-web-bluetooth',
+		'react-tag-cloud',
+		'Node.js',
+		'Web Bluetooth',
+		'famous',
+		'HTML',
+		'CSS',
+		'ES6',
+		'Async/await',
+		'eslint',
+		'Flow-Xpert',
+		'FlowX',
+		'UX',
+		'Mocha',
+		'Jest',
+		'C++',
+		'SQL',
+		'YAML',
+		'XCode',
+		'Rouwcollectie',
+		'Git',
+		'TDD',
+		'TypeScript',
+	],
+	small: ['Cordova', 'MFC', '.NET', 'VBA', 'Win-CE', 'Nijmegen'],
 };
 
 class WordCloud extends Component {
-  componentDidMount() {
-    setInterval(() => {
-      this.forceUpdate();
-    }, 20000);
-  }
+	componentDidMount() {
+		setInterval(() => {
+			this.forceUpdate();
+		}, 20000);
+	}
 
-  renderWords() {
-    const result = [];
-    for (const key in words) {
-      const style = styles[key];
-      words[key].forEach(word => {
-        result.push(
-          <Word3D
-            key={word}
-            style={style}
-            showDelay={2000 + result.length * 100}
-          >
-            {word}
-          </Word3D>
-        );
-      });
-    }
-    return result;
-  }
+	renderWords() {
+		const result = [];
+		for (const key in words) {
+			const style = styles[key];
+			words[key].forEach(word => {
+				result.push(
+					<Word3D
+						key={word}
+						style={style}
+						showDelay={2000 + result.length * 100}
+					>
+						{word}
+					</Word3D>
+				);
+			});
+		}
+		return result;
+	}
 
-  render() {
-    return (
-      <TagCloud className="cloud" style={styles.default}>
-        {this.renderWords()}
-      </TagCloud>
-    );
-  }
+	render() {
+		return (
+			<TagCloud className="cloud" style={styles.default}>
+				{this.renderWords()}
+			</TagCloud>
+		);
+	}
 }
 
 export default WordCloud;
